1be5afbd347161d1d9d91834f7e0437c3b3fc4db
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2019-01-27 14:48:14 +0100

    Add rabbitMq config

---
+++ MicroServices/MicroServices.Common/RabbitMq/RabbitMqOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RawRabbit.Configuration;
+
+namespace MicroServices.Common.RabbitMq
+{
+    public class RabbitMqOptions : RawRabbitConfiguration
+    {
+    }
+}
